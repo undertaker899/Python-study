e237c9df6bd11d055d4ea28325be884aad64ec6d
undertaker899 <80835042+undertaker899@users.noreply.github.com>
2021-03-19 22:49:45 +0300

    1st commit

--- data_types.py
+++ data_types.py
@@ -1,28 +1,36 @@
+# example
 print(round(3.14159**2/2))
 # Function (round)
 
+# example
 numbers = input()
 numbers_split = numbers.split()
 numbers_joined = '\n'.join(numbers_split)
 print(numbers_joined)
 # Methods .split and .join
 
+# example
 hours = 16
 minutes = 9
 seconds = 5
 print("%02d:%02d:%02d" % (hours, minutes, seconds))
 # String Formatting
 
+# example
 L1 = ["a", "b", "c", 1, 2, 3, 4]
 print(L1[1:4])
+
 L2 = ["a", "b", "c", 1, 2, 3, 4]
 print(L2[::3])
+
 L3 = ["a", "b", "c", 1, 2, 3, 4]
 print(L3[3::-1])
+
 L4 = ["a", "b", "c", 1, 2, 3, 4]
 print(L4[:3:-1])
 #  Slices
 
+# example
 letters = ['a', 'b', 'c', 'd']
 print(letters)
 letters.append('e')
@@ -31,24 +39,32 @@ letters.pop(3)
 print(letters)
 # Methods .append and .pop
 
+#example
 L = [3.3, 4.4, 5.5, 6.6]
 print(list(map(round, L)))
+
 L = ['3.3', '4.4', '5.5', '6.6']
 print(list(map(float, L)))
 # Function (map)
 
+# example
 string = input()
 list_of_strings = string.split()
 list_of_numbers = list(map(int, list_of_strings))
 print(sum(list_of_numbers[::3]))
 # Method .split, function (map), slices
 
+# example
 list_of_numbers = list(map(int, input().split()))
 list_of_numbers = list_of_numbers[::-1]
 list_of_numbers.append(sum(list_of_numbers))
 print(list_of_numbers)
 # Function (map), method .split and .append, slices
 
-d = {'book': input("Book:"), 'author': input("Author name:"), 'year': input("Publication date:")}
+# example
+book = input("Book:")
+author_name = input("Author name:")
+publication_date = input("Publication date:")
+d = {'book': book, 'author': author_name, 'year': publication_date}
 print(d)
 # Dictionary
