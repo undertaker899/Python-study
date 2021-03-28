295ddc33105cf98c8cd9abde5356bb87c17f494e
undertaker899 <80835042+undertaker899@users.noreply.github.com>
2021-03-28 17:06:24 +0300

    18th commit

---
+++ oop.py
@@ -0,0 +1,94 @@
+# 1
+import datetime
+
+
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
+peter = User(email="peterrobertson@mail.com", name="Peter Robertson")
+julia = User("Julia Donaldson", "juliadonaldson@mail.com")
+
+
+class Product:
+    max_quantity = 100000
+
+    def __init__(self, name, category, quantity_in_stock):
+        self.name = name
+        self.category = category
+        self.quantity_in_stock = quantity_in_stock
+
+    def is_available(self):
+        return True if self.quantity_in_stock > 0 else False
+
+
+class Food(Product):
+    is_critical = True
+    needs_to_be_refreshed = True
+    refresh_frequency = datetime.timedelta(days=1)
+
+
+eggs = Product("eggs", "food", 5)
+# Example
+
+
+# 2
+class Event:
+    def __init__(self, timestamp=0, event_type='', session_id=''):
+        self.timestamp = timestamp
+        self.type = event_type
+        self.session_id = session_id
+
+    def init_from_dict(self, event_dict):
+        self.timestamp = event_dict.get("timestamp")
+        self.type = event_dict.get("type")
+        self.session_id = event_dict.get("session_id")
+
+    def show_description(self):
+        print("This is generic event.")
+
+
+events = [
+    {
+     "timestamp": 1554583508000,
+     "type": "itemViewEvent",
+     "session_id": "0:NynteeXG:MYlskrqZbcmXNSFEJaZIsNVGeDLLpmct",
+    },
+    {
+     "timestamp": 1555296337000,
+     "type": "itemViewEvent",
+     "session_id": "0:NynteeXG:MYlskrqZbcmXNSFEJaZIsNVGeDLLpmct",
+    },
+    {
+     "timestamp": 1549461608000,
+     "type": "itemBuyEvent",
+     "session_id": "0:NynteeXG:MYlskrqZbcmXNSFEJaZIsNVGeDLLpmct",
+    },
+]
+
+
+for event in events:
+    event_obj = Event()
+    event_obj.init_from_dict(event)
+    print(event_obj.timestamp)
+
+
+class ItemViewEvent(Event):
+    type = "itemViewEvent"
+
+    def __init__(self, timestamp=0, session_id="", number_of_views=0):
+        self.timestamp = timestamp
+        self.session_id = session_id
+        self.number_of_views = number_of_views
+
+    def show_description(self):
+        print("This event means someone has browsed an item.")
+
+
+if __name__ == "__main__":
+    test_view_event = ItemViewEvent(1549461608000, "0:NynteeXG:MYlskrqZbcmXNSFEJaZIsNVGeDLLpmct", 6)
+    test_view_event.show_description()
+    print(test_view_event.type)
+# Example
