56f453f7085f24a50d02fbd93f2c74b220505b95
undertaker899 <80835042+undertaker899@users.noreply.github.com>
2021-04-07 15:54:07 +0300

    28th commit

--- practice_OOP/1.8.1/cats2.py
+++
@@ -1,7 +0,0 @@
-from cats1 import Cat
-
-cat_01 = Cat('Baron', 'Male', 2)
-cat_02 = Cat('Sam', 'Male', 2)
-print(f'Name of the cat: {cat_01.get_name()}\nSex: {cat_01.get_sex()}\nAge: {cat_01.get_age()}')
-print('------------------------')
-print(f'Name of the cat: {cat_02.get_name()}\nSex: {cat_02.get_sex()}\nAge: {cat_02.get_age()}')
